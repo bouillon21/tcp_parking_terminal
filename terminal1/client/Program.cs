168e6ad80848e7a1a55dadc3b6cc393a9f8643e9
Bulat <b.barzhanov@yandex.ru>
2021-03-28 22:38:22 +0300

    Auto stash before merge of "main" and "origin/main"

--- terminal1/client/Program.cs
+++ terminal1/client/Program.cs
@@ -45,32 +45,39 @@ namespace client
 
         static private string Exchange(string address, int port, string outMessage)
         {
-            // Инициализация
-            TcpClient client = new TcpClient(address, port);
-            Byte[] data = Encoding.UTF8.GetBytes(outMessage);
-            NetworkStream stream = client.GetStream();
             try
             {
-                // Отправка сообщения
-                stream.Write(data, 0, data.Length);
-                // Получение ответа
-                Byte[] readingData = new Byte[256];
-                String responseData = String.Empty;
-                StringBuilder completeMessage = new StringBuilder();
-                int numberOfBytesRead = 0;
-                do
+                // Инициализация
+                TcpClient client = new TcpClient(address, port);
+                Byte[] data = Encoding.UTF8.GetBytes(outMessage);
+                NetworkStream stream = client.GetStream();
+                try
                 {
-                    numberOfBytesRead = stream.Read(readingData, 0, readingData.Length);
-                    completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, numberOfBytesRead));
-                }
-                while (stream.DataAvailable);
+                    // Отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                    // Получение ответа
+                    Byte[] readingData = new Byte[256];
+                    String responseData = String.Empty;
+                    StringBuilder completeMessage = new StringBuilder();
+                    int numberOfBytesRead = 0;
+                    do
+                    {
+                        numberOfBytesRead = stream.Read(readingData, 0, readingData.Length);
+                        completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, numberOfBytesRead));
+                    }
+                    while (stream.DataAvailable);
                     responseData = completeMessage.ToString();
-                return responseData;
+                    return responseData;
+                }
+                finally
+                {
+                    stream.Close();
+                    client.Close();
+                }
             }
-            finally
+            catch (Exception)
             {
-                stream.Close();
-                client.Close();
+                return ("сервер отключен!!!!");
             }
         }
     }
