26a1b1c9b6da932a817510a366c8e2e1787bdf09
Ortaklon <nazmetdinovramil999@gmail.com>
2021-03-28 22:21:20 +0300

    Сделана отправка id на почту

--- client/Client/Form1.Designer.cs
+++ client/Client/Form1.Designer.cs
@@ -33,17 +33,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Orange;
+            this.groupBox1.BackColor = System.Drawing.Color.DarkOrange;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(-9, -1);
@@ -55,7 +59,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(97, 125);
+            this.button2.Location = new System.Drawing.Point(97, 139);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(333, 106);
             this.button2.TabIndex = 1;
@@ -65,7 +69,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 260);
+            this.button1.Location = new System.Drawing.Point(97, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(333, 97);
             this.button1.TabIndex = 0;
@@ -75,7 +79,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.BackColor = System.Drawing.Color.Orange;
+            this.groupBox2.BackColor = System.Drawing.Color.DarkOrange;
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.button4);
@@ -86,6 +90,15 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Терминал 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID";
             // 
             // textBox1
             // 
@@ -115,20 +128,28 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label2
+            // textBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 242);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "ID";
+            this.textBox2.Location = new System.Drawing.Point(97, 316);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "e-mail";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(1154, 524);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -137,6 +158,7 @@
             this.Text = "Основной терминал";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -153,6 +175,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- client/Client/Form1.cs
+++ client/Client/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Text;
+using System.Net.Mail;
+using System.Net;
 
 namespace Client
 {
@@ -62,7 +64,7 @@ namespace Client
             id = textBox1.Text;
             if (Convert.ToInt32(textBox1.Text) < 9999)
             {
-                MessageBox.Show("Предоставте к оплате " + (Exchange("127.0.0.1", 8888, "price@"+ id) + "рублей"));
+                MessageBox.Show("Предоставте к оплате " + (Exchange("127.0.0.1", 8888, "price@"+ id) + " рублей"));
             }
             else 
             {
@@ -77,7 +79,26 @@ namespace Client
 
         private void button1_Click(object sender, EventArgs e)
         {
-               MessageBox.Show("Ваш id " + Exchange("127.0.0.1", 8888, "add") + "\nЗапомните его для оплаты парковочного места");
+            string id = Exchange("127.0.0.1", 8888, "add");
+            try
+            {
+                MailAddress from = new MailAddress("4232g@mail.ru", "Parking");
+                MailAddress to = new MailAddress(textBox2.Text);
+                MailMessage m = new MailMessage(from, to);
+                m.Subject = "Чек";
+                m.Body = "Ваш id на парковке " + id;
+                SmtpClient smtp = new SmtpClient("smtp.mail.ru", 587);
+                smtp.Credentials = new NetworkCredential("4232g@mail.ru", "Qwe123asd45");
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+                MessageBox.Show($"Чек отправлен на почту {textBox2.Text}");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Произошла ошибка во время отправки");
+                Application.Exit();
+            }
+            MessageBox.Show("Ваш id " + id + "\nЗапомните его для оплаты парковочного места");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -101,6 +122,11 @@ namespace Client
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
